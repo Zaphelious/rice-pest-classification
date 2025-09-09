9853d2f215a0fda20519aed75f4f3414fff6fb8e
Zaphelious <lancestuartburdeos@gmail.com>
2025-09-10 01:45:59 +0800

    Retrain

---
+++ helpers.py
@@ -0,0 +1,149 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+def plot_training_result(*results):
+
+    full_training_loss = []
+    full_validation_loss = []
+
+    for result in results:
+        train_loss = result.get('train_loss', [])
+        full_training_loss.extend(train_loss)
+        full_validation_loss.extend(result.get('validation_loss', []))
+
+    total_epochs = len(full_training_loss)
+
+    if total_epochs == 0:
+        print("No data to plot.")
+        return
+
+    epochs = range(1, total_epochs + 1)
+
+    data = {
+        'Epoch': epochs,
+        'Training Loss': full_training_loss,
+        'Validation Loss': full_validation_loss,
+    }
+
+    df = pd.DataFrame(data)
+
+    df_long = df.melt(id_vars=['Epoch'], var_name='Loss Type', value_name='Loss')
+
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df_long, x='Epoch', y='Loss', hue='Loss Type', marker='o')
+
+    plt.title('Training and Validation Loss Comparison')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_historical_results(file_name):
+
+    data = pd.read_csv(file_name)
+
+    df = pd.DataFrame(data)
+
+    fig, axes = plt.subplots(2, 1, figsize=(10, 12))
+
+    fig.suptitle('Training and Validation Loss Comparison', fontsize=16)
+
+    historical_losses = df.melt(id_vars=['epoch'],value_vars=['train_loss', 'validation_loss'], var_name='Loss Type', value_name='Loss')
+
+    sns.lineplot(data=historical_losses, hue='Loss Type', markers='o' ,x='epoch' , y='Loss', ax=axes[0])
+
+    axes[0].set_title('Training and Validation Loss Comparison')
+    axes[0].set_xlabel('Epoch')
+    axes[0].set_ylabel('Loss')
+    axes[0].grid(True)
+    axes[0].legend()
+
+    sns.lineplot(data=df, x='epoch', y='accuracy', color='g', markers='o', ax=axes[1])
+    axes[1].set_title('Accuracy')
+    axes[1].set_xlabel('Epoch')
+    axes[1].set_ylabel('Accuracy')
+    axes[1].grid(True)
+
+    plt.tight_layout(rect=(0, 0.03, 1, 0.95))
+
+    plt.show()
+
+def plot_accuracy(*results):
+
+    full_accuracy = []
+
+    for res in results:
+        accuracy = res.get('accuracy', [])
+        full_accuracy.extend(accuracy)
+
+    total_epochs = len(full_accuracy)
+
+    if total_epochs == 0:
+        print("No accuracy data to plot.")
+        return
+
+    epochs = range(1, total_epochs + 1)
+
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(epochs, full_accuracy, label='Accuracy', marker='o', color='g')
+
+    plt.title('Accuracy Evaluation')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy (%)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def save_historical_data(history, filename):
+
+    training_loss = history.get('train_loss', [])
+    test_loss = history.get('validation_loss', [])
+    accuracy = history.get('accuracy', [])
+
+    data_dict = pd.DataFrame({
+        'train_loss': training_loss,
+        'validation_loss': test_loss,
+        'accuracy': accuracy,
+    })
+
+    if os.path.exists(filename):
+        print("historical data exists! Appending new data.")
+        old_data = pd.read_csv(filename)
+        combined_data = pd.concat([old_data, data_dict], ignore_index=True)
+
+    else:
+        print("Creating new historical data...")
+        combined_data = data_dict
+
+    combined_data['epoch'] = range(1, len(combined_data) + 1)
+
+    combined_data = combined_data[['epoch', 'train_loss', 'validation_loss', 'accuracy']]
+
+    combined_data.to_csv(filename, index=False)
+
+    print("Historical data saved successfully!")
+
+import cv2
+from torchvision import datasets
+
+class CustomDataset(datasets.ImageFolder):
+
+    def __getitem__(self, index):
+
+        path, target = self.samples[index]
+
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented['image']
+
+        return image, target
+
+    def __len__(self):
+        return len(self.samples)
